a03ade7a068681a2e1f74a2481b174c349b61c0c
RaduPopescu95 <73418692+RaduPopescu95@users.noreply.github.com>
2024-10-31 13:11:58 +0000

    adaugatverificare search

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -8,7 +8,7 @@ import BannerPost from "@/components/blog/BannerPost";
 import Blog2 from "@/components/blog/Blog2";
 
 export const metadata = {
-  title: "Grid With Sidebar || Jano - Creative Multipurpose React NextJS Template",
+  title: "Web App Dynamicx | Blog",
 };
 
 const GridWithSidebar = () => {
